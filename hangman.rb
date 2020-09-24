2f16ca6a2a0b73d599248d937bcaa230432283aa
CHANG-CHING-CHUNG <kangan987@gmail.com>
2020-09-24 15:28:33 +0800

    Some functions added

--- hangman.rb
+++ hangman.rb
@@ -1,6 +1,11 @@
 class Hangman
   @@word_list;
   @@guess_counter = 5;
+  @@secret_word;
+  @@correct_letters = [];
+  @@incorrect_letters = [];
+  @@anwser = [];
+  @@random_num;
   def initialize()
     file = File.open("5desk.txt");
     newfile = file.readlines.map(&:chomp);
@@ -9,20 +14,47 @@ class Hangman
     file.close;
   end
 
-  def generate3randomNum
-    random = (0..11).to_a.shuffle;
-    threeNums = random[0..2];
-    puts threeNums.inspect;
+  def generate3randomNum(length)
+    random = (0..length - 1).to_a.shuffle;
+    @@random_num = random[0..2];
   end
 
   def generateRandomWord
     length = @@word_list.length;
     randomNum = (0..length - 1).to_a.shuffle;
     randomNum = randomNum.pop;
-    word = @@word_list[randomNum];
-    puts word;
+    word = @@word_list[randomNum].split("");
+    @@secret_word = word;
+  end
+ 
+  def createSecretWord
+    length = self.generateRandomWord().length;
+    self.generate3randomNum(length);
+    @@random_num.each_with_index {
+      |num, i|
+      @@anwser.push(@@secret_word[num]);
+      @@secret_word[num] = '_';
+    }
+    puts @@anwser.inspect;
+    puts @@secret_word.inspect;
+  end
+
+  def getUserInput
+    puts "Guess a letter"
+    inputLength = 0;
+    userInput = 'a';
+    begin
+      if inputLength > 1 || !userInput.match(/[a-z]/)
+        puts "One letter at a time!";
+        puts "Please enter your letter again.";
+      end
+      userInput = gets.chomp.downcase;
+      inputLength = userInput.length;
+    end while inputLength != 1 || !userInput.match(/[a-z]/);
+    puts userInput;
   end
 end
 
 a = Hangman.new;
-a.generateRandomWord();
+a.createSecretWord();
+a.getUserInput();
