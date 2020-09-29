c6905f0cabe271ed0d8503205e9f0aab52689fdd
CHANG-CHING-CHUNG <54635661+CHANG-CHING-CHUNG@users.noreply.github.com>
2020-09-29 23:11:34 +0800

    Updated

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,5 @@
+require 'json';
+
 class Hangman
   @@word_list;
   @@guess_counter = 5;
@@ -6,6 +8,7 @@ class Hangman
   @@correct_letters = [];
   @@incorrect_letters = [];
   @@chosen_letters = [];
+  @@letter_position = [];
   @@random_num;
   @@userInput;
   def initialize()
@@ -47,10 +50,16 @@ class Hangman
   def getUserInput
     puts "Guess a letter"
     inputLength = 0;
-    @@userInput = 'a';
-    begin
-      if inputLength > 1 || !@@userInput.match(/[a-z]/)
+    @@userInput = gets.chomp.downcase;
+    inputLength = @@userInput.length;
+    while inputLength != 1 || !@@userInput.match(/[a-z]/) || self.checkChosenLetter();
+      if inputLength != 1
+        puts inputLength;
         puts "One letter at a time!";
+        puts "Please enter your letter again.";
+      elsif !@@userInput.match(/[a-z]/)
+        puts inputLength;
+        puts "Enter A-Z letters only.";
         puts "Please enter your letter again.";
       elsif self.checkChosenLetter()
         puts "The #{@@userInput} has been chosen before";
@@ -58,26 +67,28 @@ class Hangman
       end
       @@userInput = gets.chomp.downcase;
       inputLength = @@userInput.length;
-    end while inputLength != 1 || !@@userInput.match(/[a-z]/) || self.checkChosenLetter();
+    end
     @@guess_counter = @@guess_counter - 1;
-    @@userInput = "";
     puts @@userInput;
   end
 
   def checkUserInput
-    if @@full_secret_word.include?(@@userInput);
-      @@full_secret_word.each_with_index {
-        |char, i|
-        if @@userInput === char
-          @@secret_word[i] = @@userInput;
-        end
-      }
+    isCorrect = false;
+    @@letter_position.each_with_index {
+      |pos, i|
+      if @@full_secret_word[pos].downcase === @@userInput
+        @@secret_word[pos] = @@userInput;
+        isCorrect = true;
+      end
+    }
+
+    if isCorrect
       @@correct_letters.push(@@userInput);
-      @@chosen_letters.push(@@userInput);
-    else
+    else 
       @@incorrect_letters.push(@@userInput);
-      @@chosen_letters.push(@@userInput);
     end
+    @@chosen_letters.push(@@userInput);
+    
   end
 
   def displayStas
@@ -85,7 +96,9 @@ class Hangman
     puts 'Incorrect',@@incorrect_letters.inspect;
     puts 'Chosen',@@chosen_letters.inspect;
     puts 'Secret',@@secret_word.inspect;
+    puts 'full_secret_word',@@full_secret_word.inspect;
     puts 'Counter',@@guess_counter.inspect;
+    puts 'Letter Position',@@letter_position.inspect;
   end
 
   def checkChosenLetter
@@ -94,13 +107,120 @@ class Hangman
     end
     return false;
   end
+
+  def getLetterPosition
+    @@secret_word.each_with_index {
+      |letter, i|
+      if letter === "_"
+        @@letter_position.push(i);
+      end
+    }
+  end
+
+  def checkWin
+    win = true;
+    @@secret_word.each_with_index {
+      |char, i|
+      if @@secret_word[i] != @@full_secret_word[i]
+        win = false;
+      end
+    }
+    if @@guess_counter === 0
+      win = false;
+      puts "Game Over";
+      puts "Would you like to play next round? Y/N";
+      begin
+        case (continue = gets.chomp.downcase)
+        when "y"
+          self.clearAll();
+          self.gameStart();
+        when "n"
+          puts "Good Bye!";
+          return !win;
+        else 
+          puts "Y or N only."
+        end
+      end while continue != "y" || continue != "n";
+    elsif win
+      puts "You win!";
+      puts "Play again? Y/N";
+      begin
+        case (continue = gets.chomp.downcase)
+        when "y"
+          self.clearAll();
+          self.gameStart();
+        when "n"
+          puts "Good Bye!";
+          return win;
+        else 
+          puts "Y or N only."
+        end
+      end while continue != "y" || continue != "n";
+    else 
+      return win
+    end
+  end
+
+  def clearAll
+    @@word_list;
+    @@guess_counter = 5;
+    @@secret_word;
+    @@full_secret_word= [];
+    @@correct_letters = [];
+    @@incorrect_letters = [];
+    @@chosen_letters = [];
+    @@letter_position = [];
+    @@random_num;
+    @@userInput;
+  end
+
+  def gameStart
+    self.createSecretWord();
+    self.getLetterPosition();
+    begin
+      if self.saveGame()
+        puts "See you~!";
+        return 
+      end
+      self.loadStatusFromJSON(self.to_JSON);
+      self.getUserInput();
+      self.checkUserInput();
+      self.displayStas();
+    end while !self.checkWin()
+  end
+
+  def saveGame
+    puts "If you want to save the game and quit, enter 'Y' ";
+    
+    begin
+      isYes = gets.chomp.downcase;
+      case (isYes)
+      when "y"
+        self.to_JSON();
+        return true
+      when "n"
+        return false;
+      end
+    end while isYes != "y" || isYes != "n";
+  end
+
+  def to_JSON
+    JSON.dump ({
+     :guess_counter => @@guess_counter,
+     :secret_word => @@secret_word,
+     :full_secret_word => @@full_secret_word,
+     :correct_letters => @@correct_letters,
+     :incorrect_letters => @@incorrect_letters,
+     :chosen_letters => @@chosen_letters,
+     :letter_position => @@letter_position,
+    })
+  end
+
+  def loadStatusFromJSON(string)
+    gameStatus = JSON.load string
+    puts gameStatus.inspect;
+  end
 end
 
 a = Hangman.new;
-a.createSecretWord();
-a.getUserInput();
-a.checkUserInput();
-a.displayStas();
-a.getUserInput();
-a.checkUserInput();
-a.displayStas();
+a.gameStart();
